e02833526352acb1a949f7def754d591b8f97cef
Lesslie Arantxa Sosa Gonzalez <lesslie.sosa@Lesslies-MAC.mshome.net>
2017-10-10 11:44:14 -0500

    Test for Jenkins last

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ var app = require('../app.js');
  
 describe('GET /', function() {
   it('respond with hello world', function(done) {
-    request(app).get('/').expect('hello jenkins for Damian', done);
+    request(app).get('/').expect('Hello jenkins for Damian', done);
   });
 });
